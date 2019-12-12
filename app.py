3cd6c2b3a2f567e6b75430a4f7a3fb42107ea5c2
Arjun Nair <nair.arjun47@yahoo.com>
2019-12-12 08:53:20 -0800

    added all the other routes

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 from flask_login import LoginManager
 
 from resources.stocks import stock
+from resources.etfs import etf
+from resources.bonds import bond
 from resources.users import user
 
 import models
@@ -22,9 +24,12 @@ CORS(app)
 app.secret_key = "s234aIoda2IJN"
 login_manager.init_app(app)
 
-# @login_manager.user_loader
-# def load_user(userid):
-#     try: 
+@login_manager.user_loader
+def load_user(userid):
+    try: 
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 
 @app.before_request
@@ -43,6 +48,9 @@ def after_request(response):
 CORS(stock, origins=['http://localhost:3000'],supports_credentials=True)
 
 app.register_blueprint(stock, url_prefix='/api/v1/stocks')
+app.register_blueprint(stock, url_prefix='/api/v1/etfs')
+app.register_blueprint(stock, url_prefix='/api/v1/bonds')
+app.register_blueprint(user, url_prefix='/api/v1/users')
 
 if __name__ == '__main__':
     models.initialize()
